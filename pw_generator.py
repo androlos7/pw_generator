5bd6c80dfc4cea374b6703981cda39d8ab7a6156
androlos7 <132691515+androlos7@users.noreply.github.com>
2023-08-04 11:00:06 +0200

    Update pw_generator.py
    - Password Num

--- pw_generator.py
+++ pw_generator.py
@@ -1,13 +1,21 @@
 import random
 import string
 
-def generate_password(length=12):
+def generate_password(length=12, num_passwords=1):
+    # Mögliche Zeichen für das Kennwort
     characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for _ in range(length))
-    return password
+
+    passwords = []
+    for _ in range(num_passwords):
+        # Zufällige Zeichen auswählen
+        password = ''.join(random.choice(characters) for _ in range(length))
+        passwords.append(password)
+
+    return passwords
 
 if __name__ == "__main__":
     password_length = int(input("Geben Sie die Länge des Kennworts ein (Standardmäßig 12): "))
     password_num = int(input("Wieviele Kennwörter benötigen Sie:"))
     password = generate_password(password_length, password_num)
-    print("Generiertes Kennwort:", password)
+
+print("Generiertes Kennwort:", password)
